f56640a06c50b0b37f789fcbfb6310eb0851475c
Antonio Grigolini <grig.griganto@gmail.com>
2018-02-27 19:54:16 +0100

    descrizione bandiere ok

--- spree_grigoprinting_ecommerce/app/models/spree/line_item_decorator.rb
+++ spree_grigoprinting_ecommerce/app/models/spree/line_item_decorator.rb
@@ -7,12 +7,25 @@ module Spree
         more_opt_h = JSON.parse(more_options, {:symbolize_names => true})
         # aggiungo nei parametri la quantita per il ricalcolo del prezzo
         more_opt_h[:quantity] = quantity
-        if more_opt_h #&& more_opt_h[:personalizzazione] && more_opt_h[:personalizzazione] == "bandiera_personalizzata"
+        if more_opt_h && more_opt_h[:personalizzazione] && more_opt_h[:personalizzazione] == "bandiera_personalizzata"
           variant.price = Spree::CalcolatorePrezzo.calcola_bandiera(more_opt_h).to_d
         end
       end
 
       self.price = variant.price_including_vat_for(tax_zone: tax_zone)
     end
+
+    def description
+      desc = variant.description
+      if more_options
+        more_opt_h = JSON.parse(more_options, {:symbolize_names => true})
+        if more_opt_h && more_opt_h[:personalizzazione] && more_opt_h[:personalizzazione] == "bandiera_personalizzata"
+          desc = "in nautico 110g cm "+more_opt_h[:base]+"x"+more_opt_h[:altezza]+
+          "\nFiniture: varie"
+        end
+      end
+      return desc
+    end
+
   end
 end
