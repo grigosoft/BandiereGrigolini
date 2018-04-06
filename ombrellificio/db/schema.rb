97a4e1467caf9eb1318eceb0f3418b9a9d6f1034
Antonio Grigolini <grig.griganto@gmail.com>
2018-04-06 17:48:08 +0200

    fix merge addresses

--- ombrellificio/db/schema.rb
+++ ombrellificio/db/schema.rb
@@ -930,7 +930,6 @@ ActiveRecord::Schema.define(version: 20180406154007) do
     t.string "last_sign_in_ip"
     t.string "login"
     t.integer "ship_address_id"
-    t.integer "bill_address_id"
     t.string "authentication_token"
     t.string "unlock_token"
     t.datetime "locked_at"
@@ -946,7 +945,7 @@ ActiveRecord::Schema.define(version: 20180406154007) do
     t.string "ragione_sociale"
     t.string "piva"
     t.string "cf"
-    t.index ["bill_address_id"], name: "index_spree_users_on_bill_address_id"
+    t.integer "bill_address_id"
     t.index ["deleted_at"], name: "index_spree_users_on_deleted_at"
     t.index ["email"], name: "email_idx_unique", unique: true
     t.index ["ship_address_id"], name: "index_spree_users_on_ship_address_id"
