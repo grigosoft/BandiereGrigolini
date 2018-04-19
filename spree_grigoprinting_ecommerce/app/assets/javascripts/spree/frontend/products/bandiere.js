8ecb30d8f56898f342f2222de18c002ccfc10579
Antonio Grigolini <grig.griganto@gmail.com>
2018-04-19 15:02:03 +0200

    selected mostra e nascondi partial finiture nella pagina bandiere

--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/bandiere.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/bandiere.js
@@ -1,97 +1,113 @@
-function bindWithActive(classIdentifierGroup){
-  $(classIdentifierGroup).click(selezionaTessuto);
-  function selezionaTessuto(){
+function bindSelezioni(){
+  $('[data-product-selection]').click(function(){
     var active = $(this);
-    $(classIdentifierGroup).removeClass('active');
-    active.addClass('active');
-  }
-}
-function showPartialFromData(){
-  // orientamento
-  if($('#orientamento').val() == 'orizzontale'){
-    $('.att_verticale').addClass('hidden');
-    $('.att_orizzontale').removeClass('hidden');
-  } else if($('#orientamento').val() == 'verticale'){
-    $('.att_orizzontale').addClass('hidden');
-    $('.att_verticale').removeClass('hidden');
-  } else {
-    $('.att_orizzontale').addClass('hidden');
-    $('.att_verticale').addClass('hidden');
-  }
-  // finitura fettuccia/manica
-  if($('#finitura').val() == 'manica'){
-    $('.att_fettuccia').addClass('hidden');
-    $('.att_manica').removeClass('hidden');
-  } else if($('#finitura').val() == 'fettuccia'){
-    $('.att_manica').addClass('hidden');
-    $('.att_fettuccia').removeClass('hidden');
-  } else {
-    $('.att_manica').addClass('hidden');
-    $('.att_fettuccia').addClass('hidden');
-  }
-}
-function showPartialFromState(stato){
-  var visibili = stato.split('.');
-  // nascondo tutti
-  $('.hide_all').addClass('hidden');
-  // mostro
-  for(att in visibili){
-    $('show_'+att).removeClass('hidden');
-  }
-}
-
-function setInMoreOptions(comando){
-  var moreOptionsTag = $('more_options');
-  var moreOptionsValue = JSON.parse(moreOptionsTag.val());
-  var attributi = comando.split('.');
-  var navigatore = moreOptionsValue;
-  for(att in attributi){
-    next = navigatore[att];
-    if(next == null){
-
+    // aggiorno show_state
+    var old_state = $('#show_state').val();
+    $('#show_state_old').val(old_state);
+    old_state = old_state.split('.');
+    var new_state = "";
+    for(var i=0;i<active.data('product-state')-1; i++){
+      new_state += old_state[i]+'.';
     }
+    new_state += active.data('product-options');
+    $('#show_state').val(new_state);
+    showPartialFromState();
+    setActiveFromState();
+    controllaDati();
+  });
+}
+function showPartialFromState(){
+  var visibili = $('#show_state').val().split('.');
+  var visibiliOld = $('#show_state_old').val().split('.');
+  // trovo il punto in comune
+  var comPoint = 0;
+  while((comPoint<visibili.length || comPoint<visibiliOld.length) && visibili[comPoint]==visibiliOld[comPoint]){
+    comPoint++;
+  }
+  // nascondo fino al punto in comune
+  for(var i=visibiliOld.length-1; i>=comPoint; i--){
+    $("div[data-need-show~='{}']".replace('{}',visibiliOld[i])).addClass('hidden');
+    if(i>0)
+      $("div[data-need-show~='{}']".replace('{}',visibiliOld[i-1]+'.'+visibiliOld[i])).addClass('hidden');
+  }
+  // mostro nuovi
+  for(var i=comPoint; i<visibili.length; i++){
+    $("div[data-need-show~='{}']".replace('{}',visibili[i])).removeClass('hidden');
+    if(i>0)
+      $("div[data-need-show~='{}']".replace('{}',visibili[i-1]+'.'+visibili[i])).removeClass('hidden');
   }
 }
+function setActiveFromState(){
+  // tolgo tutti gli active
+  $(".selected").removeClass("selected");
+  // metto quelli presenti
+  var visibili = $('#show_state').val().split('.');
+  for(var i in visibili){
+    $("[data-product-options='{}']".replace('{}', visibili[i])).addClass('selected');
+  }
+}
+// funzione che riconosce se sono stati fatte tutte le scelte relative alla finitura
+function isFinitureSelezionate(){
+  // trovo componente show_state e prendo attributo completed
+}
+function setInMoreOptions(active){
+
+}
+// funzioni di salvataggio dati nel campo more options
+// var optionIniector;
+// optionIniector['nome'] = function(nome){
+//   var moreOptionsTag = $('more_options');
+//   var moreOptionsValue = {};
+//   try {
+//     moreOptionsValue = JSON.parse(moreOptionsTag.val());
+//   } catch(err){console.error(err.message);}
+//   moreOptionsValue['nome'] = nome;
+//   moreOptionsTag.val(JSON.stringify(moreOptionsValue));
+// }
+
+
+
+function controllaDati(){
+
+}
+// function setInMoreOptions(active){
+//   var moreOptionsTag = $('more_options');
+//   var moreOptionsValue = {};
+//   try {
+//     moreOptionsValue = JSON.parse(moreOptionsTag.val());
+//   } catch(err){log.console.error(err.message);}
+//   var temp = active.data('product-options').split('->');
+//   var percorso = temp[0].split('/');
+//   var replaceData = JSON.parse(temp[1]);
+//   var navigatore = moreOptionsValue;
+//   for(var i=0;i<percorso.length()-1; i++){
+//
+//   }
+//   for(step in percorso){
+//     if(navigatore[step] == null){
+//
+//     }
+//     navigatore = navigatore[step];
+//   }
+// }
 
 $(document).ready(function(){
   // bind gruppi di scelta
-
-  // bind scelta tessuto
-  bindWithActive('.scelta_tessuto');
-  // bind scelta orizzontali/verticali
-  bindWithActive('.scelta_orientamento');
-  // bind scelta finitura lato asta
-  bindWithActive('.scelta_finitura_lato_asta');
-  // bind scelta tipo manica
-  bindWithActive('.scelta_tipo_manica');
-  // bind scelta acessori fettuccia
-  bindWithActive('.scelta_acessori_fettuccia');
+  bindSelezioni();
+  // // bind scelta tessuto
+  // bindWithActive('.scelta_tessuto');
+  // // bind scelta orizzontali/verticali
+  // bindWithActive('.scelta_orientamento');
+  // // bind scelta finitura lato asta
+  // bindWithActive('.scelta_finitura_lato_asta');
+  // // bind scelta tipo manica
+  // bindWithActive('.scelta_tipo_manica');
+  // // bind scelta acessori fettuccia
+  // bindWithActive('.scelta_acessori_fettuccia');
 
   // pagina appena caricata nascondo personalizzazioni
-  showPartialFromData();
-
-// da cambiare e far prendere il valore dai dati html
-// unificare in una funzione
-  // orizzontale/verticale
-  $('#btn_orizzontale').click(function(){
-    $('input#orientamento').val("orizzontale");
-    showPartialFromData();
-  });
-  $('#btn_verticale').click(function(){
-    $('input#orientamento').val("verticale");
-      $('input#finitura').val("fettuccia");
-    showPartialFromData();
-  });
-  // manica/fettuccia/(personalizzata)
-  $('#btn_manica').click(function(){
-    $('input#finitura').val("manica");
-    showPartialFromData();
-  });
-  $('#btn_fettuccia').click(function(){
-    $('input#finitura').val("fettuccia");
-    showPartialFromData();
-  });
-
-
-// cambiare active in base all'hidden data
+  $('[data-need-show]').addClass('hidden');
+  $('#show_state_old').val("");
+  showPartialFromState();
+  setActiveFromState()
 });
