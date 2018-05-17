c4b4f60fc14dd6d8f2d139e53b86e81464da7f31
Antonio Grigolini <grig.griganto@gmail.com>
2018-05-17 16:26:32 +0200

    + drag fulari

--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
   $( "#colori_scelti" ).sortable({
-    revert: true
+    revert: true,
+    update: generaFilare
   });
   $( ".draggable" ).draggable({
     connectToSortable: "#colori_scelti",
     helper: "clone",
     revert: "invalid"
   });
-  $( "ul, li" ).disableSelection();
+
 });
+function generaFilare(){
+  var i;
+  var listaColori = [];
+  var imgScelte = $('#colori_scelti').find('div>a>img');
+  for(i=0; i<imgScelte.length; i++){
+    listaColori.push($(imgScelte[i]).attr('colore'));
+  }
+  var filare = '';
+  var maxBandierine = 30;
+  var iColore;
+  for(i=0; i<maxBandierine; i++){
+    iColore = i%listaColori.length;
+    filare +=   '<img alt="Colore {colore}" src="{src}">'.replace('{colore}',listaColori[iColore]).replace('{src}',$('img[colore='+listaColori[iColore]+']').attr('src'));
+  }
+  $('#filare_generato').attr('style',"width: "+maxBandierine*50+'px')
+  $('#filare_generato').html(filare);
+}
 //
 // // drag and drop
 // function allowDrop(ev) {
