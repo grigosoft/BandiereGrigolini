ada8a4c7c381ae2f15b90a75ea75dbc56e2f3095
Antonio Grigolini <grig.griganto@gmail.com>
2018-05-17 14:14:03 +0200

    fix draggable in filari

--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
-  $('img[draggable="true"]').draggable();
+  $( "#colori_scelti" ).sortable({
+    revert: true
+  });
+  $( ".draggable" ).draggable({
+    connectToSortable: "#colori_scelti",
+    helper: "clone",
+    revert: "invalid"
+  });
+  $( "ul, li" ).disableSelection();
 });
-
-// drag and drop
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("colore", $(ev.target).attr('colore'));
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("colore");
-    $(ev.target).append($('a>img[colore='+data+']').clone());
-}
+//
+// // drag and drop
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
+//
+// function drag(ev) {
+//     ev.dataTransfer.setData("colore", $(ev.target).attr('colore'));
+// }
+//
+// function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("colore");
+//     $(ev.target).append($('a>img[colore='+data+']').clone());
+// }
--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/spree_grigoprinting_ecommerce.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/spree_grigoprinting_ecommerce.js
@@ -1,6 +1,7 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 //= require jquery
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require dropzone
 
