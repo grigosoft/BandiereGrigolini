c1572015ab20a13b7ec0c3547eab94b4040f36a5
Antonio Grigolini <grig.griganto@gmail.com>
2018-05-21 15:17:27 +0200

    filari con colori dinamici

--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/filari.js
@@ -2,7 +2,7 @@ function f_toMoreOptions(){
   prodotto.moreOptions['formato'] = $('.selected[data-product-selection="formato"]').attr('data-product-options');
   // estrappolo lista colori
   var listaColori = [];
-  var imgScelte = $('#colori_scelti').find('div>a>img');
+  var imgScelte = $('#colori_scelti>div');
   for(var i=0; i<imgScelte.length; i++){
     listaColori.push($(imgScelte[i]).attr('colore'));
   }
@@ -10,7 +10,7 @@ function f_toMoreOptions(){
   // quanti
   var soggetti=0;
   for(i=0; i<listaColori.length; i++){// solo se contiene una personalizzata attivo gli extra
-    if(listaColori[i] == 'personalizzata'){
+    if(listaColori[i].startsWith('personalizzato')){
       soggetti ++;
     }
   }
@@ -30,8 +30,8 @@ function f_fromMoreOptions(){
 function f_visualiza(){
   var select_misura = $('select#misura_bandierine');
   // select_misura.html('');
-  var misure = [['1','2'],
-                ['1','2','3','4']];
+  var misure = [['20x30','25x35'],
+                ['15x15','15x21','20x30','25x35']];
   var options = [];
   if(prodotto.moreOptions['formato'] == 'triangolare'){
     for(var i=0;i<misure[0].length; i++) {
@@ -57,15 +57,6 @@ $(document).ready(function(){
   prodotto.fromMoreOptions.push(f_fromMoreOptions);
   prodotto.visualizza.push(f_visualiza);
   prodotto.fireFromMoreOptions();
-  $( "#colori_scelti" ).sortable({
-    revert: true,
-    update: function(){prodotto.fireToMoreOptions()}
-  });
-  $( ".draggable" ).draggable({
-    connectToSortable: "#colori_scelti",
-    helper: "clone",
-    revert: "invalid"
-  });
 
   // selezioni
   $('[data-product-selection="formato"]').click(function(){
@@ -75,39 +66,62 @@ $(document).ready(function(){
   });
   // aggiungi soggetto personalizzato
   $('#aggiungi_bandierina_personalizzata').click(function(){
-
+    $("#scegli_colori").insertAt($("#scegli_colori").children().length-1, creaSvgPersonalizzato());
+    rendiDraggable("#scegli_colori>.draggable");
   });
   // crea bandierine
   var filePiatto = $('#bandierina_piatta').attr('href');
-  var colori = [['Rosso','#ff0000'],'Blu','giallo','verde','cyan','arancio','nero','bianco'];
+  // var colori = [['Rosso','#ff0000'],'Blu','Giallo','Verde','Cyan','Arancio','Nero','Bianco'];
+  var colori = [['Rosso','#ff0000'],['Blu','#0000ff'],['Giallo','#ffff00'],['Verde','#00cc66'],['Cyan','#00ffff'],['Arancio','#ff9933'],['Nero','#000000'],['Bianco','#ffffff']];
   for(var i=0; i<colori.length; i++) {
-    creaSvgPiatto(filePiatto, colori[i][1], colori[i][0]);
+    $("#scegli_colori").insertAt(i, creaSvgPiatto(colori[i][1], colori[i][0]));
   }
+  $( "#colori_scelti" ).sortable({
+    revert: true,
+    update: function(){prodotto.fireToMoreOptions()}
+  });
+  rendiDraggable("#scegli_colori>.draggable");
 });
-
-function creaSvgPiatto(file, colore, nome){
-  var head = '<div class="draggable col-xs-3 col-md-2 button-options" role="button">';
-  var foot = '<p>'+nome+'</p></div>';
-  var svg = $('#bandierina_piatta').svg();
-  alert(svg);
-  
+function rendiDraggable(sel){
+  $( sel ).draggable({
+    connectToSortable: "#colori_scelti",
+    helper: "clone",
+    revert: "invalid"
+  });
+}
+function creaSvgPiatto(colore, nome){
+  var head = '<div class="draggable col-xs-3 col-md-2" colore="'+nome.toLowerCase()+'"><a class="button-options" role="button"><div>';
+  var foot = '</div><p>'+nome+'</p></a></div>';
+  var svg = $('#bandierina_piatta').html();
+  svg = svg.replace(/st0{fill:#[0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F]/gi, "st0{fill:"+colore);
+  svg = svg.replace(/st0/g,'st_'+nome);
+  return head+svg+foot;
+}
+function creaSvgPersonalizzato(){
+  var nPersonalizzate = $('#scegli_colori>[colore^="personalizzato"]').length;
+  var head = '<div class="draggable col-xs-3 col-md-2" colore="personalizzato_'+nPersonalizzate+'"><a class="button-options" role="button"><div>';
+  var foot = '</div><p>P: '+nPersonalizzate+'</p></a></div>';
+  var svg = $('#bandierina_personalizzata').html();
+  svg = svg.replace(/class="st2 st3">1/gi, 'class="st2 st3">'+nPersonalizzate);
+  // svg = svg.replace(/st/g,'st_p_');
+  return head+svg+foot;
 }
 function generaFilare(){
   var i;
-  if(prodotto.moreOptions['sequenza_colori'] == null || prodotto.moreOptions['sequenza_colori'].length == 0) {
-    return;
-  }
-  var listaColori = prodotto.moreOptions['sequenza_colori'].split('.');
-  var filare = '';
-  var maxBandierine = 30;
-  var iColore;
-  for(i=0; i<maxBandierine; i++){
-    iColore = i%listaColori.length;
-    filare +=   '<img alt="Colore {colore}" src="{src}">'.replace('{colore}',listaColori[iColore]).replace('{src}',$('img[colore='+listaColori[iColore]+']').attr('src'));
-  }
-  $('#filare_generato').attr('style',"width: "+maxBandierine*50+'px')
-  $('#filare_generato').html(filare);
+  var listaColori = prodotto.moreOptions['sequenza_colori'];
+  if(listaColori != null && listaColori.length > 0) {
+    listaColori = listaColori.split('.');
+    var filare = '';
+    var maxBandierine = 30;
+    var iColore;
+    for(i=0; i<maxBandierine && listaColori.length>0; i++){
+      iColore = i%listaColori.length;
+      filare += '<div>'+$('#scegli_colori>div[colore="'+listaColori[iColore]+'"]>a>div').html()+'</div>';
+    }
+    $('#filare_generato').attr('style',"width: "+maxBandierine*50+'px')
+    $('#filare_generato').html(filare);
 
-  // clear stili clonati
-  $('#colori_scelti>div').attr('style','')
+    // clear stili clonati
+    $('#colori_scelti>div').attr('style','')
+  }
 }
