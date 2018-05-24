73b72fb3a12090c3f7672dc0b37c5cb3adc74cc1
Antonio Grigolini <grig.griganto@gmail.com>
2018-05-24 15:07:01 +0200

    bandiere istituzionali

--- spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/bandiere_istituzionali.js
+++ spree_grigoprinting_ecommerce/app/assets/javascripts/spree/frontend/products/bandiere_istituzionali.js
@@ -1,3 +1,18 @@
+function visualizza(){
+  var ricerca = $('#ricerca').val().toLowerCase();
+  if(ricerca != null && ricerca.length > 0) {
+    $('div[stato]').addClass('hide');
+    $('div[stato^="'+$('#ricerca').val()+'"]').removeClass('hide');
+  } else {
+    $('div[stato].hide').removeClass('hide');
+  }
+}
 $(document).ready(function(){
-  
+  $('#ricerca').change(visualizza);
+  $('#ricerca').keyup(visualizza);
+
+  $('[data-product-radio="stati"]').click(function(){
+    $('[data-product-radio="stati"]').removeClass('selected');
+    $(this).addClass('selected');
+  });
 });
